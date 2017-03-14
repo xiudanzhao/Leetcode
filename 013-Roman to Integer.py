00974f57a7252e03db1bd116d4d6faa474ceafbd
DannyZhaoo <XiudanZhao@Outlook.com>
2017-03-14 09:39:38 +0800

    have finished 16 problems

--- 013-Roman to Integer.py
+++ 013-Roman to Integer.py
@@ -12,11 +12,32 @@ class Solution(object):
         while i<len(a):
             d[b[i]]=a[i]
             i += 1
-        while len(s)>0:
-            if s[:2] in d:
-                ans += d[s[:2]]
-                s=s[2:]
-            else:
-                ans += d[s[:1]]
-                s=s[1:]
-        return ans
+            while len(s)>0:
+                if s[:2] in d:
+                    ans += d[s[:2]]
+                    s=s[2:]
+                else:
+                    ans += d[s[:1]]
+                    s=s[1:]
+                    return ans
+
+
+
+        #anothor way to solve this problem
+        class Solution(object):
+            def romanToInt(self, s):
+            """
+            :type s: str
+            :rtype: int
+            """
+            d = {'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+            ans = 0
+            size = len(s)
+            i = 0
+            while i < size:
+                if i > 0 and d[s[i]] > d[s[i - 1]]:
+                    ans += d[s[i]] - 2 * d[s[i - 1]]
+                else:
+                    ans += d[s[i]]
+                i += 1
+            return ans
