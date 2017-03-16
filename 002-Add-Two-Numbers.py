d99e97f41b5ceeb320f6e474272b4fc972eba81f
DannyZhaoo <XiudanZhao@Outlook.com>
2017-03-16 19:56:55 +0800

    重新加上第二题

--- 002-Add-Two-Numbers.py
+++ 002-Add-Two-Numbers.py
@@ -1,9 +1,3 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
-
 class Solution(object):
     def addTwoNumbers(self, l1, l2):
         #头指针
